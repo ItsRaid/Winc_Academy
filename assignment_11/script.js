970b35201c68dd20898cc3de2473903163983f37
Raid <74338235+ItsRaid@users.noreply.github.com>
2021-12-07 16:07:15 +0100

    Delete assignment_11 directory

--- assignment_11/script.js
+++
@@ -1,9 +0,0 @@
-let name = 'Raid';
-let numbers = 4 + 4;
-let strings = "4 + 4";
-let addition = 12 + 4;
-let subtraction = 20 - 4;
-let multiplication = 4 * 4;
-let division = 64 / 4;
-let modulus = 5 % 2;
-let age = 30;

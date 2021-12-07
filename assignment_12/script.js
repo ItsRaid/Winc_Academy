79e41bceb8a25106442a89372176216b4b47f078
Raid <74338235+ItsRaid@users.noreply.github.com>
2021-12-07 17:55:28 +0100

    Exercise: use if-else statements

---
+++ assignment_12/script.js
@@ -0,0 +1,21 @@
+const age = 30;
+const isFemale = true;
+const driverStatus = 'bob';
+
+if (age >= 18) {
+    console.log("You are allowed to enter.")
+} else {
+    console.log("You are too young, we kindly request you to stay outside.")
+}
+
+if (isFemale) {
+    console.log("You are a female.")
+} else {
+    console.log("You are not a female.")
+}
+
+if (driverStatus == 'bob') {
+    console.log("You are allowed to drive!")
+} else {
+    console.log("You are not allowed to drive!")
+}

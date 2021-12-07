7f18fa7d34028f9f6081e2e0a233f5e56dcf79a6
Raid <74338235+ItsRaid@users.noreply.github.com>
2021-12-07 16:30:01 +0100

    Exercise: Run a JS program on your computer + make comments

---
+++ JS-exercise/script.js
@@ -0,0 +1,10 @@
+console.log('Hello Winc Academy students')
+
+// This is a large sum
+/*
+let numbers = 1230941 * 1823794;
+console.log(numbers);
+
+let division = 637263 / 54;
+console.log(division);
+*/

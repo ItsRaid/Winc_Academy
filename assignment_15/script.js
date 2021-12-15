cb81c84050e7a2a83bcecbc127fdbbb3de36cc91
Raid <74338235+ItsRaid@users.noreply.github.com>
2021-12-15 17:25:22 +0100

    Function basics: exercise

---
+++ assignment_15/script.js
@@ -0,0 +1,18 @@
+const makePizza = function () {
+    console.log("Prepare your dough");
+    console.log("Add tomato sauce and toppings");
+    console.log("Slide your pizza in the oven")
+};
+
+const makeSushi = function () {
+    console.log("Cook your rice");
+    console.log("Place your cooked rice on seaweed");
+    console.log("Add your desired ingredients on top of your rice");
+    console.log("Roll your sushi, and slice it up");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
